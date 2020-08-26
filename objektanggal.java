6d7e8fd99f3e9c24f802aa190d16b7a6145ad939
luluandii <70243837+luluandii@users.noreply.github.com>
2020-08-26 09:03:21 +0800

    Create objektanggal.java

---
+++ objektanggal.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class ObjekTanggal {
+  public static void main (String[] args){
+    Date waktuSekarang = new Date();
+    System.out.println( "Isi waktuSekarang: " + waktuSekarang);
+    }
+}
