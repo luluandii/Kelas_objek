03284009d8716cfe66190b7b9586edd2f9d2021c
luluandii <70243837+luluandii@users.noreply.github.com>
2020-08-26 09:11:07 +0800

    Create PemanggilanMetode.java

---
+++ PemanggilanMetode.java
@@ -0,0 +1,8 @@
+public class PemanggilanMetode {
+  public static void main(String[]args{
+    String st = "Selamat Belajar Java";
+    
+    System.out.println(st.toUpperCase());
+    System.out.println(st.charAt(5));
+   }
+}
