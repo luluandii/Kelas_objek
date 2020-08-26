c79f83fdb6915e8a674800ae5f2c67d1a2aa89f0
luluandii <70243837+luluandii@users.noreply.github.com>
2020-08-26 09:18:13 +0800

    Update KelasMobil.java

--- KelasMobil.java
+++ KelasMobil.java
@@ -2,4 +2,14 @@ class Mobil {
   String warna;
   int tahunProduksi;
 }
-public class KelasMobil
+public class KelasMobil {
+  public static void main(String[]args{
+    Mobil mobilku = new Mobil();
+    
+    mobilku.warna = "Merah";
+    mobilku.tahunProduksi = 2019;
+    
+    System.out.println("Warna : " + mobilku,warna);
+    System.out.println("Tahun : " + mobilku.tahunProduksi);
+   }
+}
